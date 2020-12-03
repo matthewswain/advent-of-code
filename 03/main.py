3f566d4d1b232471cd551035a36d29da121210f7
Matthew Swain <matthewswain@gmail.com>
2020-12-03 11:34:10 +0000

    Day 3.

---
+++ 03/main.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+
+from functools import reduce
+
+
+def get_map(path):
+    with open(path) as in_f:
+        map_ = [line.strip() for line in in_f.readlines()]
+        return map_
+
+
+def is_tree(map_, v_pos, h_pos):
+    h_pos = h_pos % len(map_[0])
+    return map_[v_pos][h_pos] == "#"
+
+
+def navigate(map_, right, down):
+    h_pos = 0
+    trees = 0
+    for v_pos in range(0, len(map_), down):
+        if is_tree(map_, v_pos, h_pos):
+            trees += 1
+        h_pos += right
+    return trees
+
+
+if __name__ == "__main__":
+    map_ = get_map("03/data")
+
+    trees = navigate(map_, 3, 1)
+    print(trees)
+
+    paths = [
+        (1, 1),
+        (3, 1),
+        (5, 1),
+        (7, 1),
+        (1, 2),
+    ]
+    trees = [navigate(map_, p[0], p[1]) for p in paths]
+    print(reduce(lambda a, b: a * b, trees))
