d6d65ecaf7ecce7b81b383be99d41d5963764c75
Matthew Swain <matthewswain@gmail.com>
2020-12-06 11:31:51 +0000

    Day 6.

---
+++ 2020/06/main.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+
+import string
+from functools import reduce
+
+
+def get_data(path):
+    with open(path) as in_f:
+        data = in_f.read()
+        for group in data.split("\n\n"):
+            yield [list(p) for p in group.split("\n") if p]
+
+
+def count_unique_answers(group):
+    answers = set([answer for person in group for answer in person])
+    return len(answers)
+
+
+def keep_matching(list_a, list_b):
+    return [x for x in list_a if x in list_b]
+
+
+def count_unanimous_answers(group):
+    unanimous = reduce(keep_matching, group, string.ascii_lowercase)
+    return len(unanimous)
+
+
+def calculate_total_score(groups, count_func):
+    scores = list([count_func(g) for g in groups])
+    return reduce(lambda x, y: x + y, scores)
+
+
+if __name__ == "__main__":
+    groups = list(get_data("data"))
+
+    # Part 1
+    total = calculate_total_score(groups, count_unique_answers)
+    print(total)
+
+    # Part 2
+    total = calculate_total_score(groups, count_unanimous_answers)
+    print(total)
