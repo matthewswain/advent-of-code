37f8bde1d21acbfb92eb3127417f7d9acdb853ae
Matthew Swain <matthew.swain@zoopla.co.uk>
2020-12-22 11:34:02 +0000

    Day 14, part 2.

--- 2020/14/main.py
+++ 2020/14/main.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
 
 
-from typing import Dict, List
+from itertools import combinations
+from typing import Dict, List, Set
 
 
 def load_data(path: str):
@@ -47,11 +48,79 @@ class Program:
         return (number & off) | on
 
 
+class ProgramV2:
+    memory: Dict[int, int]
+    bitmask: str
+
+    def __init__(self):
+        self.memory = {}
+        self.bitmask = "0" * 36
+
+    def update_mask(self, bitmask: str):
+        self.bitmask = bitmask
+
+    def update_memory(self, address: int, value: int):
+        addresses = self.apply_mask(self.bitmask, address)
+        for address in addresses:
+            self.memory[address] = value
+
+    def step(self, instruction: str):
+        k, v = instruction.split(" = ")
+        if k.startswith("mem"):
+            address = int(k[4 : len(k) - 1])
+            self.update_memory(address, int(v))
+        elif k == "mask":
+            self.update_mask(v)
+        else:
+            raise ValueError(f"Unexpected instruction: {instruction}")
+
+    def run(self, instructions: List[str]):
+        for instruction in instructions:
+            self.step(instruction)
+
+    def total(self) -> int:
+        return sum(self.memory.values())
+
+    @staticmethod
+    def apply_mask(bitmask: str, number: int) -> Set[int]:
+        numbers = set()
+
+        on = int(bitmask.replace("X", "0"), 2)
+        number = number | on
+        numbers.add(number)
+
+        masks = []
+        for i, c in enumerate(bitmask):
+            if c == "X":
+                mask = 2 ** (len(bitmask) - (i + 1))
+                masks.append((mask, True))
+                masks.append((~mask, False))
+
+        mask_combinations = []
+        for i in range(1, len(masks) + 1):
+            list(map(lambda x: mask_combinations.append(x), combinations(masks, i)))
+
+        for combination in mask_combinations:
+            masked_number = number
+            for mask, on in combination:
+                if on:
+                    masked_number = masked_number | mask
+                else:
+                    masked_number = masked_number & mask
+            numbers.add(masked_number)
+
+        return numbers
+
+
 if __name__ == "__main__":
     instructions = load_data("data")
 
+    # Part 1
     program = Program()
     program.run(instructions)
+    print(program.total())
 
-    # Part 1
+    # Part 2
+    program = ProgramV2()
+    program.run(instructions)
     print(program.total())
--- 2020/14/main_test.py
+++ 2020/14/main_test.py
@@ -1,4 +1,4 @@
-from main import Program
+from main import Program, ProgramV2
 
 
 def test_apply_mask():
@@ -34,3 +34,20 @@ def test_program():
     program.run(instructions)
     assert program.memory[7] == 101
     assert program.memory[8] == 64
+
+
+def test_apply_mask_v2():
+    cases = [
+        {
+            "bitmask": "000000000000000000000000000000X1001X",
+            "number": 42,
+            "expected": set([26, 27, 58, 59]),
+        },
+        {
+            "bitmask": "00000000000000000000000000000000X0XX",
+            "number": 26,
+            "expected": set([16, 17, 18, 19, 24, 25, 26, 27]),
+        },
+    ]
+    for case in cases:
+        assert case["expected"] == ProgramV2.apply_mask(case["bitmask"], case["number"])
