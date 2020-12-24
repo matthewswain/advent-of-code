6f76b6a4c1c586a8d2547069fa6a536f5d7f0128
Matthew Swain <matthew.swain@zoopla.co.uk>
2020-12-24 14:46:29 +0000

    Day 18, part 2.

--- 2020/18/main.py
+++ 2020/18/main.py
@@ -8,12 +8,12 @@ def load_data(path):
         return list([l.strip() for l in in_f.readlines()])
 
 
-def eval_line(line):
+def eval_line_1(line):
     line = line.replace(" ", "")
 
     while matches := re.findall(r"\(([0-9\*\+]+)\)", line):
         for m in matches:
-            line = line.replace(f"({m})", str(eval_line(m)))
+            line = line.replace(f"({m})", str(eval_line_1(m)))
 
     total = 0
     operator = "+"
@@ -30,8 +30,26 @@ def eval_line(line):
     return total
 
 
+def eval_line_2(line):
+    line = line.replace(" ", "")
+
+    while matches := re.findall(r"\(([0-9\*\+]+)\)", line):
+        for m in matches:
+            line = line.replace(f"({m})", str(eval_line_2(m)))
+
+    while m := re.search(r"([0-9]+(?:\+[0-9]+)+)", line):
+        e = m.groups()[0]
+        pattern = r"(?<![0-9]){}(?![0-9])".format(e.replace("+", r"\+"))
+        line = re.sub(pattern, str(eval(e)), line)
+
+    return eval(line)
+
+
 if __name__ == "__main__":
     data = load_data("data")
 
     # Print 1
-    print(sum([eval_line(l) for l in data]))
+    print(sum([eval_line_1(l) for l in data]))
+
+    # Part 2
+    print(sum([eval_line_2(l) for l in data]))
--- 2020/18/main_test.py
+++ 2020/18/main_test.py
@@ -1,9 +1,29 @@
-from main import eval_line
+from main import eval_line_1, eval_line_2
 
 
-def test_eval_line():
+def test_eval_line_1():
     cases = [
         {"input": "7 * 8 + 2 + 8 * (8 * 4) * (4 + 8)", "expected": 25344},
     ]
     for case in cases:
-        assert eval_line(case["input"]) == case["expected"]
+        assert eval_line_1(case["input"]) == case["expected"]
+
+
+def test_eval_line_2():
+    cases = [
+        {"input": "1 + (2 * 3) + (4 * (5 + 6))", "expected": 51},
+        {"input": "2 * 3 + (4 * 5)", "expected": 46},
+        {"input": "5 + (8 * 3 + 9 + 3 * 4 * 3)", "expected": 1445},
+        {"input": "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", "expected": 669060},
+        {"input": "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", "expected": 23340},
+        {"input": "2 + 4", "expected": 6},
+        {"input": "2 * 4", "expected": 8},
+        {"input": "10", "expected": 10},
+        {
+            "input": "3 + 6 * (7 * 8 * 6 * (5 + 8 + 6) + 5 * 4) * ((8 + 9 + 6 * 8) * 9 + 6 * 6 + 2 * 2) * 4",
+            "expected": 51279298560,
+        },
+        {"input": "17 + 7 * 2 * 117 + 71", "expected": 9024},
+    ]
+    for case in cases:
+        assert eval_line_2(case["input"]) == case["expected"]
